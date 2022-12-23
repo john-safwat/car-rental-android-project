90b962f6883932aa8c2c5830b8123a006fd499a1
john safwat <john362sss@gmail.com>
2022-12-23 12:31:04 +0200

    cange the car card widget

--- lib/UI/user_interface/taps/showcarwidget.dart
+++ lib/UI/user_interface/taps/showcarwidget.dart
@@ -58,60 +58,92 @@ class Show_car_Widget extends StatelessWidget {
               )
             ]
           ),
-          padding: EdgeInsets.only(top: 20 , left: 10),
-          height:mediaQuere.height *0.22 ,
-          child: Row(
+          padding: EdgeInsets.only(top:20 ,),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Image.network(car.Image),
-                ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("${car.ManufacturCompany} ${car.CarModel}" , style: Theme.of(context).textTheme.headline1?.copyWith(fontSize: 24),textAlign: TextAlign.center,),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.airline_seat_recline_normal_rounded , color: MyTheme.primarycolor,),
-                          Text(": ${car.Seats}" ,style: TextStyle(fontSize: 18),)
-                        ],
-                      ),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.speed_rounded , color: MyTheme.primarycolor,),
-                          Text(": ${car.MaximumSpeed} Kmh" ,style: TextStyle(fontSize: 18),)
-                        ],
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 15),
-                        child: ElevatedButton(
-                              onPressed: (){
-                                Navigator.pushNamed(context, PaymentPage.routeName , arguments: car);
-                              },
-                              style: ButtonStyle(
-                                padding: MaterialStateProperty.all(EdgeInsets.all(15)),
-                                  backgroundColor:MaterialStateProperty.all(MyTheme.primarycolor),
-                                  fixedSize: MaterialStateProperty.all(Size.fromWidth(mediaQuere.width),),
-                                shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomRight: Radius.circular(20), topLeft: Radius.circular(20))) )
-                              ),
-                              child: Text('Rent Now' , style: TextStyle(fontSize: 20),)
-                          ),
-                      )
-                    ],
+              Text("${car.ManufacturCompany} ${car.CarModel}" , style: Theme.of(context).textTheme.headline1?.copyWith(fontSize:30),textAlign: TextAlign.center,),
+              const SizedBox(height: 10,),
+              Image.network(car.Image , height: mediaQuere.height*0.2, fit: BoxFit.contain,),
+              const SizedBox(height: 10,),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text('${car.Price} EGP' , style: Theme.of(context).textTheme.headline1?.copyWith(fontSize:22),textAlign: TextAlign.center),
                   ),
-                ),
+                  Expanded(
+                      child: ElevatedButton(
+                        onPressed: (){
+                          Navigator.pushNamed(context, PaymentPage.routeName , arguments: car);
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(MyTheme.primarycolor),
+                          padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+                          shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20),
+                          )))
+                        ),
+                        child: Text('Rent Now' , style: TextStyle(fontSize: 20),)
+                      ),
+                  )
+                ],
               ),
             ],
           ),
+
+          // child: Row(
+          //   children: [
+          //     Expanded(
+          //       flex: 2,
+          //       child: Container(
+          //         child: Image.network(car.Image),
+          //       ),
+          //     ),
+          //     Expanded(
+          //       flex: 3,
+          //       child: Container(
+          //         child: Column(
+          //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //           children: [
+          //             Text("${car.ManufacturCompany} ${car.CarModel}" , style: Theme.of(context).textTheme.headline1?.copyWith(fontSize: 24),textAlign: TextAlign.center,),
+          //
+          //             Row(
+          //               mainAxisAlignment: MainAxisAlignment.center,
+          //               children: [
+          //                 Icon(Icons.airline_seat_recline_normal_rounded , color: MyTheme.primarycolor,),
+          //                 Text(": ${car.Seats}" ,style: TextStyle(fontSize: 18),)
+          //               ],
+          //             ),
+          //
+          //             Row(
+          //               mainAxisAlignment: MainAxisAlignment.center,
+          //               children: [
+          //                 Icon(Icons.speed_rounded , color: MyTheme.primarycolor,),
+          //                 Text(": ${car.MaximumSpeed} Kmh" ,style: TextStyle(fontSize: 18),)
+          //               ],
+          //             ),
+          //             Container(
+          //               margin: EdgeInsets.only(left: 15),
+          //               child: ElevatedButton(
+          //                     onPressed: (){
+          //                       Navigator.pushNamed(context, PaymentPage.routeName , arguments: car);
+          //                     },
+          //                     style: ButtonStyle(
+          //                       padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+          //                         backgroundColor:MaterialStateProperty.all(MyTheme.primarycolor),
+          //                         fixedSize: MaterialStateProperty.all(Size.fromWidth(mediaQuere.width),),
+          //                       shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomRight: Radius.circular(20), topLeft: Radius.circular(20))) )
+          //                     ),
+          //                     child: Text('Rent Now' , style: TextStyle(fontSize: 20),)
+          //                 ),
+          //             )
+          //           ],
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // ),
         ),
       ),
     );
